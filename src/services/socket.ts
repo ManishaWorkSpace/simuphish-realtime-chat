db0da90852538c199ffb744dde0baf0a7618268d
ManishaWorkSpace <yadavmanisha3611@gmail.com>
2026-02-14 11:03:08 +0530

    new socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -4,15 +4,10 @@ let socket: Socket;
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
-      transports: ["websocket"],
+    socket = io("/", {
+  transports: ["websocket"],
+});
 
-      reconnection: true,
-      reconnectionAttempts: 5,
-      reconnectionDelay: 1000,
-
-      autoConnect: true,
-    });
   }
 
   return socket;
